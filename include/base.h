ec677321703456b304ddb7b990708e12af90a09a
Robert Weche <bobbyzto@users.noreply.github.com>
2024-10-09 02:35:54 -0400

    create base header

---
+++ include/base.h
@@ -0,0 +1,31 @@
+/***************************************************************
+* 
+* Utility code incl. 
+*   - Helper macros
+*   - Atomic type aliases
+*   - Misc. general purpose code
+*
+* NOTE:
+*
+* (!)   Code written here should conform to ANSI C or C99
+*   standard at the latest. See 'Engineering problem solving 
+*   with ANSI C' by Delores M. Etter', and 'Modern C 
+*   Programming' by Orhan Gazi for reference.
+*
+*
+***************************************************************/
+
+#ifndef BASE_H_INCLUDED
+#define BASE_H_INCLUDED
+
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
+// Code here
+
+#ifdef __cplusplus
+    }
+#endif    
+
+#endif // BASE_H
