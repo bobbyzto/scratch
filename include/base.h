d9ae878b8cab997d9ce436c599864e1c31cdba9d
Robert Weche <bobbyzto@users.noreply.github.com>
2025-03-06 19:51:38 -0500

    Added wordle project

--- include/base.h
+++ include/base.h
@@ -18,6 +18,25 @@
 #ifndef BASE_H_INCLUDED
 #define BASE_H_INCLUDED
 
+#include <stddef.h>
+
+// Atomics
+typedef uint8_t     u8;
+typedef char16_t   c16;
+typedef int32_t    b32;
+typedef int32_t    i32;
+typedef uint32_t   u32;
+typedef uint64_t   u64;
+typedef float      f32;
+typedef double     f64;
+typedef uintptr_t uptr;
+typedef char      byte;
+typedef ptrdiff_t size;
+typedef size_t   usize;
+
+// Macros
+#define countof(a) (size)(sizeof(a) / sizeof(*(a)))
+
 #ifdef __cplusplus
     extern "C" {
 #endif
